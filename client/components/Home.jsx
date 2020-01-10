94dbae5df3eee636d712fde7254e7f2713dfe208
ethan-t-obrien <ethanobrien1009@gmail.com>
2020-01-10 16:08:01 +1300

    intiail setup

---
+++ client/components/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+
+
+render() {
+  return (
+    <div className="home">
+      <div className="home__circle">
+        <div className="home__circle--img">
+
+        </div>
+      <div className="home__text">
+        
+      </div>
+      </div>
+    </div>
+  )
+}
+}
+
+export default Home
