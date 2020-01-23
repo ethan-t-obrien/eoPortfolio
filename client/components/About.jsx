873e8c66477026d941c1975cb216a849bc5ab1ab
Rautini Thompson <Rautini Thompson>
2020-01-23 20:06:36 +1300

    add about info

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -10,9 +10,11 @@ class About extends React.Component {
 
   render() {
     return (
-   <>
+   <body>
    <h1>About Me</h1>
-   </>
+   <h3 className="about">I am web devloper and artist, based in Otaki.</h3>
+   <p className="graph">I'm a person who loves to tackle and overcome problems as well as build and create anything beautiful and intuitive. If i'm not at my computer coding, I'll be spending time with my whanau, drawing, or playing games.</p>
+   </body>
     )
   }
 }
